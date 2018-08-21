475f46cdf0c9ddb276e54bf9c4eb80c8e315ccbf
Ramasamy Seenivasagan <r.seenivasagan@lakeba.com>
2018-08-21 06:25:14 +0530

    Added products

--- app/src/main/java/com/rams/appollo/codeswag/Services/DataService.kt
+++ app/src/main/java/com/rams/appollo/codeswag/Services/DataService.kt
@@ -22,25 +22,36 @@ object DataService {
     )
 
     val hats = listOf(
-            Products("graphic beanie","$10","hat01"),
-            Products("graphic beanie2","$10","hat02"),
-            Products("graphic beanie3","$10","hat03"),
-            Products("graphic beanie4","$10","hat04")
+            Products("graphic beanie","$10","hat1"),
+            Products("graphic beanie2","$10","hat2"),
+            Products("graphic beanie3","$10","hat3"),
+            Products("graphic beanie4","$10","hat4")
     )
 
     val hoodies = listOf(
-            Products("graphic beanie","$10","hoodie01"),
-            Products("graphic beanie2","$10","hoodie02"),
-            Products("graphic beanie3","$10","hoodie03"),
-            Products("graphic beanie4","$10","hoodie04")
+            Products("graphic beanie","$10","hoodie1"),
+            Products("graphic beanie2","$10","hoodie2"),
+            Products("graphic beanie3","$10","hoodie3"),
+            Products("graphic beanie4","$10","hoodie4")
     )
 
     val shirts = listOf(
-            Products("graphic shirts","$10","shirt01"),
-            Products("graphic shirts2","$10","shirt02"),
-            Products("graphic shirts3","$10","shirt03"),
-            Products("graphic shirts4","$10","shirt04"),
-            Products("graphic shirts5","$10","shirt05")
+            Products("graphic shirts","$10","shirt1"),
+            Products("graphic shirts2","$10","shirt2"),
+            Products("graphic shirts3","$10","shirt3"),
+            Products("graphic shirts4","$10","shirt4"),
+            Products("graphic shirts5","$10","shirt5")
     )
+
+    val digitalGood = listOf<Products>()
+
+    fun getProducts(category: String) : List<Products>{
+        return when(category){
+            "SHIRTS" -> shirts
+            "HATS" -> hats
+            "HOODIES" -> hoodies
+            else -> digitalGood
+        }
+    }
 
 }
